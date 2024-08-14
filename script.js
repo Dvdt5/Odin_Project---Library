c5feef827c9b33f01c12ec03d67d113d4654ad56
Davut Dagtekin <dagtekindavut07@gmail.com>
2024-08-14 14:39:44 +0300

    Stored books in an array, now can add books, read-notread buttons works now

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ const confirmFormBtn = document.getElementById("confirm-form-btn");
 const toggleReadedStatusBtn = document.querySelectorAll(".toggle-readed-btn");
 
 //Assign Elements
-const addBookForm = document.getElementById("popup-modal");
+const addBookModal = document.getElementById("popup-modal");
 const modalShade = document.getElementById("shade");
+const bookContainer = document.getElementById("middle-column")
+const addBookForm = document.getElementById("popup-form");
 
 
 //Assign input elements
@@ -15,42 +17,115 @@ const bookAuthorInput = document.getElementById("author-input");
 const bookNumberInput = document.getElementById("pagenumber-input");
 const bookReadInput = document.getElementById("readed-checkbox-btn");
 
+//Books will be stored here
+let listOfBooks = [
+    {
+        title: "Hunger Games",
+        author: "SUzanne Collins",
+        pageNumber: "384",
+        isReaded: true
+    },
+    {
+        title: "Lord of the Flies",
+        author: "William Golding",
+        pageNumber: "225",
+        isReaded: true
+    },
+    {
+        title: "Harry Potter and the Philosopher's Stone",
+        author: "J. K. Rowling",
+        pageNumber: "342",
+        isReaded: false
+    },
+    {
+        title: "Great Expectations",
+        author: "Charles Dickens",
+        pageNumber: "640",
+        isReaded: false
+    }
+];
 
+//Clearing the values from form when closing and submitting it
 const clearInputValues = () => {
     bookTitleInput.value = "";
     bookAuthorInput.value = "";
     bookNumberInput.value = "";
 }
 
+//Adding readed or not button to all the books
+const addReadedButton = () => {
+    document.querySelectorAll(".toggle-readed-btn").forEach(function(button) {
+        button.addEventListener("click", ()=>{
+            if (button.classList.contains("readed")){
+                button.classList.remove("readed");
+                button.classList.add("notreaded");
+                button.textContent = "NOT READED";
+                button.nextElementSibling.classList.toggle("triangle-notreaded");
+            } else {
+                button.classList.remove("notreaded");
+                button.classList.add("readed");
+                button.textContent = "READED";
+                button.nextElementSibling.classList.toggle("triangle-notreaded");
+            }
+            
+        });
+      });
+};
+
+const addBook = () => {
+    listOfBooks.push({
+        title: bookTitleInput.value,
+        author: bookAuthorInput.value,
+        pageNumber: bookNumberInput.value,
+        isReaded: bookReadInput.checked
+    })
+    displayBooks();
+}
+
+const displayBooks = () => {
+    bookContainer.innerHTML = "";
+    listOfBooks.map(({title,author,pageNumber,isReaded})=>{
+        bookContainer.innerHTML += `
+         <div class="book-tile">
+            <p class="book-title">${title}</p>
+            <p class="book-author">${author}</p>
+            <p class="book-pagenumber">${pageNumber}</p>
+            <div>
+                <button type="button" class="${isReaded ? "readed": "notreaded"} toggle-readed-btn">${isReaded ? "READED": "NOTREADED"}</button>
+                <div class="triangle ${isReaded ? "" : "triangle-notreaded"}"></div>
+            </div>
+        </div>`;
+    })
+    
+    addReadedButton();
+};
+
+
+
 
 addBookBtn.addEventListener("click", ()=>{
-    addBookForm.style.display = "block"
+    addBookModal.style.display = "block"
     modalShade.style.display = "block"
     addBookBtn.style.pointerEvents = "none";
 });
 
 closeFormBtn.addEventListener("click", ()=>{
     clearInputValues();
-    addBookForm.style.display = "none";
+    addBookModal.style.display = "none";
     modalShade.style.display = "none"
     addBookBtn.style.pointerEvents = "all";
 });
 
-toggleReadedStatusBtn.forEach(function(button) {
-    button.addEventListener("click", ()=>{
-        if (button.classList.contains("readed")){
-            button.classList.remove("readed");
-            button.classList.add("notreaded");
-            button.textContent = "NOT READED";
-            button.nextElementSibling.style.borderLeft = "red 12.5px solid";
-            button.nextElementSibling.style.borderRight = "red 12.5px solid";
-        } else {
-            button.classList.remove("notreaded");
-            button.classList.add("readed");
-            button.textContent = "READED";
-            button.nextElementSibling.style.borderLeft = "greenyellow 12.5px solid";
-            button.nextElementSibling.style.borderRight = "greenyellow 12.5px solid";
-        }
-        
-    });
-  });
+
+
+addBookForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    addBook();
+    displayBooks();
+    clearInputValues();
+    addBookModal.style.display = "none";
+    modalShade.style.display = "none"
+    addBookBtn.style.pointerEvents = "all";
+});
+
+displayBooks();
